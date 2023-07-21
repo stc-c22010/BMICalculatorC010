dfa0ff352c8b0a80fc0e7db4377648af14db0593
stc-c22010 <c22010@suntech.jp>
2023-07-22 00:15:13 +0900

    計算機能実装完了 クリア機能実装完了

--- app/src/main/java/jp/suntech/c22010/myapplication/MainActivity.java
+++ app/src/main/java/jp/suntech/c22010/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.suntech.c22010.myapplication;
 
+import static java.lang.Float.parseFloat;
 import static java.lang.String.*;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,48 +31,50 @@ public class MainActivity extends AppCompatActivity {
             TextView out_obesity = findViewById(R.id.tv_obesity);
             TextView out_show_best_weight = findViewById(R.id.tv_show_best_weight);
             TextView out_best_weight = findViewById(R.id.tv_best_weight);
+            TextView out_best_weight_kg = findViewById(R.id.tv_best_weight_kg);
 
             int id = view.getId();
 
+            //計算ボタンが押されたら
             if(id == R.id.bt_calc){
-
-
-
-                //計算機能バグってます
-
-
-
-                float height = Float.parseFloat(in_height.getText().toString());
-                float weight = Float.parseFloat(in_weight.getText().toString());
+                if(parseFloat(in_age.getText().toString()) < 16){
+                    //ダイアログ表示
+                    CalcDialogFragment dialogFragment = new CalcDialogFragment();
+                    dialogFragment.show(getSupportFragmentManager(), "CalcDialogFragment");
+                }
+                float height = parseFloat(in_height.getText().toString());
+                float weight = parseFloat(in_weight.getText().toString());
                 float bmi = weight * 10000 / height / height;
                 float best_weight = weight * 22 / bmi;
 
 
                 //判定を表示
-                out_show_obesity.setText(R.string.tv_obesity);
+                out_obesity.setText(R.string.tv_obesity);
                 if(bmi < 18.5){
-                    out_obesity.setText(R.string.tv_obesity_low);
+                    out_show_obesity.setText(R.string.tv_obesity_low);
                 }
                 else if(bmi < 25){
-                    out_obesity.setText(R.string.tv_obesity_nom);
+                    out_show_obesity.setText(R.string.tv_obesity_nom);
                 }
                 else if(bmi < 30){
-                    out_obesity.setText(R.string.tv_obesity_1);
+                    out_show_obesity.setText(R.string.tv_obesity_1);
                 }
                 else if(bmi < 35){
-                    out_obesity.setText(R.string.tv_obesity_2);
+                    out_show_obesity.setText(R.string.tv_obesity_2);
                 }
                 else if(bmi < 40){
-                    out_obesity.setText(R.string.tv_obesity_3);
+                    out_show_obesity.setText(R.string.tv_obesity_3);
                 }
                 else{
-                    out_obesity.setText(R.string.tv_obesity_4);
+                    out_show_obesity.setText(R.string.tv_obesity_4);
                 }
 
                 //適正体重を表示
-                out_show_best_weight.setText(R.string.tv_best_weight);
-                out_best_weight.setText(format("%.1f", valueOf(best_weight)));
+                out_best_weight.setText(R.string.tv_best_weight);
+                out_show_best_weight.setText(format("%.1f", best_weight));
+                out_best_weight_kg.setText(R.string.tv_weight2);
             }
+            //クリアボタンが押されたら
             else if(id == R.id.bt_clr){
                 in_age.setText("");
                 in_height.setText("");
@@ -80,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 out_obesity.setText("");
                 out_show_best_weight.setText("");
                 out_best_weight.setText("");
+                out_best_weight_kg.setText("");
             }
         }
     }
